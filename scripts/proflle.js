5ff992ea5af8005461e9d1014a5cc569a68c4e2b
loraineamaral <loraineeamaral@gmail.com>
2019-03-27 18:08:31 -0300

    posting text

---
+++ scripts/proflle.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function() {
+
+    $("#exit").click(function (event) {
+        event.preventDefault();
+
+        firebase.auth().signOut().then(function() {
+            window.location = 'index.html';
+        }).catch(function(error) {
+            alert("Erro: " + error);
+        });
+    });
+
+    $("#post-button").click(function(){
+        let postText = $("#post-text").val();
+        $("#post-feed").prepend(
+          `<li class="post-style"> ${postText} </li>
+           <button>Excluir</button>
+           <button>Editar</button>
+          `);
+      });
+      
+
+});
+
