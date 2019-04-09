582e4d4bd3cee85978843c2e9d0d485c687ab0e7
loraineamaral <loraineeamaral@gmail.com>
2019-04-09 18:32:37 -0300

    filter posts

--- scripts/profile.js
+++ scripts/profile.js
@@ -9,11 +9,12 @@ $(document).ready(function () {
 
         let childKey = childSnapshot.key;
         let childData = childSnapshot.val();
+        let privacy = childData.privacyType;
+        let time = childData.time
 
-        createPost(childData.text, childKey)
+        createPost(childData.text, childKey, privacy, time)
       });
     });
-
 
   $("#post-button").click(function (event) {
     event.preventDefault();
@@ -21,18 +22,20 @@ $(document).ready(function () {
     let postText = $("#post-text").val();
     $("#post-text").val("");
 
+    let privacy = $("#privacy-menu option:selected").text();
+    let hour = getTime()
+
     let newPostInDB = database.ref('posts/' + USER_ID).push({
-      text: postText
+      text: postText,
+      privacyType: privacy,
+      time: hour
     });
 
-    createPost(postText, newPostInDB.key);
+    createPost(postText, newPostInDB.key, privacy, hour);
     $("#post-button").prop("disabled", true);
-
   });
 
-
   $("#post-button").prop("disabled", true);
-
 
   $("#post-text").keyup(function (event) {
     if ($(this).val().length != 0) {
@@ -43,11 +46,14 @@ $(document).ready(function () {
     }
   });
 
-  function createPost(text, key) {
+  function createPost(text, key, privacyType, hour) {
     $("#post-feed").prepend(
       `<li class="post-style" data-post-id="${key}"> 
-      <div class="d-flex flex-row justify-content-between mr-5 my-3 p-3 border border-secondary rounded"> 
-          <p data-id-post="${key}" class="post-text">${text}</p> 
+      <div class="d-flex flex-row justify-content-between mr-5 my-3 p-3 border border-secondary rounded">
+      <div class="d-flex flex-column align-item-end">
+      <p data-id-post="${key}" class="post-text">${text}</p> 
+      <span class="privacy-type">${privacyType}-${hour}</span>
+      </div>      
           <div class= "btns d-flex flex-column align-items-start"> 
           <button class="btn-del ml-auto" id="btn-del" data-del-id="${key}"><i class="fas fa-trash-alt"></i></button>
           <button class="btn-edit ml-auto" id="btn-edit" data-edit-id="${key}"><i class="fas fa-edit"></i></button>
@@ -64,7 +70,6 @@ $(document).ready(function () {
       database.ref("posts/" + USER_ID + "/" + key).remove();
     });
 
-
     $(`button[data-edit-id=${key}]`).click(function (event) {
       event.preventDefault();
 
@@ -78,14 +83,13 @@ $(document).ready(function () {
           text: newText
         })
 
-      $(`[data-id-txta=${key}]`).replaceWith(`<p data-id-post="${key}" class="post-text">${newText}</p>`)
-        
-      $(`.btn-save[data-save-id="${key}"]`).toggleClass("d-none");
+        $(`[data-id-txta=${key}]`).replaceWith(`<p data-id-post="${key}" class="post-text">${newText}</p>`)
+
+        $(`.btn-save[data-save-id="${key}"]`).toggleClass("d-none");
       });
     });
 
   }
-
 
   $("#exit").click(function (event) {
     event.preventDefault();
@@ -96,5 +100,22 @@ $(document).ready(function () {
     });
   });
 
+$("button").click(function (click){
+      event.preventDefault();
+      let id = button.id;
+  console.log(id)
+
 });
 
+});
+
+function getTime() {
+  let tweetDate = new Date();
+  let tweethour = tweetDate.getHours().toString();
+  let tweetMin = tweetDate.getMinutes().toString();
+  if (tweetMin.length < 2) {
+    tweetMin = "0" + tweetMin;
+  }
+  let printHour = tweethour + ":" + tweetMin;
+  return printHour;
+}
